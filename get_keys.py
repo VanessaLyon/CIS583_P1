c14c3ccac096daba8f5dc055ccf2b3ba28ce846b
VanessaLyon <62832343+VanessaLyon@users.noreply.github.com>
2024-01-28 12:21:54 -0500

    Update get_keys.py
    seems reasonable

--- get_keys.py
+++ get_keys.py
@@ -1,59 +1,72 @@
 from web3 import Web3
-import eth_account
 import os
+import binascii
+import mnemonic
+from mnemonic import Mnemonic
+import random
+import string
 
-def get_mnemonic(account):
-    """Retrieve the mnemonic for an Ethereum account."""
-    try:
-        mnemonic = account.from_key(account.key).mnemonic
-        return mnemonic
-    except ValueError:
-        print("Account does not have a valid mnemonic.")
-        return None
 
-def store_mnemonic(mnemonic, filename="eth_mnemonic.txt"):
-    """Store the mnemonic in a file."""
-    with open(filename, 'a') as file:
-        file.write(mnemonic + '\n')
+def generate_random_mnemonic():
+    """Generate a random 12-word mnemonic phrase."""
+    word_list = [
+        "apple", "banana", "cherry", "date", "elderberry", "fig", "grape", 
+        "honeydew", "kiwi", "lemon", "mango", "nectarine", "orange", "papaya", 
+        "quince", "raspberry", "strawberry", "tangerine", "grapefruit", 
+        "blueberry", "melon", "coconut", "apricot", "pear", "peach", "plum", 
+        "lychee", "lime", "jackfruit", "guava", "pomegranate", "pineapple"
+    ]
 
-def get_keys(challenge, keyId=0, filename="eth_mnemonic.txt"):
+    # Shuffle and select 12 words from the word list
+    selected_words = random.sample(word_list, 12)
+
+    # Join the selected words to create the mnemonic phrase
+    mnemonic_phrase = ' '.join(selected_words)
+
+    return mnemonic_phrase
+
+def get_keys(challenge, keyId, filename="eth_mnemonic.txt"):
     """
-    Generate a stable private key and sign a message.
+    Generate a stable private key, convert it to a mnemonic, and sign a message.
     challenge - byte string
     keyId (integer) - which key to use
     filename - filename to read and store mnemonics
     """
 
     w3 = Web3()
-
-    # Enable unaudited HD wallet features
-    Account.enable_unaudited_hdwallet_features()
+    w3.eth.account.enable_unaudited_hdwallet_features()
 
     # Check if we have enough mnemonics in the file
+    mnemonics = []
     try:
         with open(filename, 'r') as file:
             mnemonics = file.readlines()
     except FileNotFoundError:
         mnemonics = []
 
-    # If we need more mnemonic, generate and save them
+    print('lenght of file is ', len(mnemonics))
+
+    # If we need more mnemonics, generate and save them
     if keyId >= len(mnemonics):
-        acct = eth_account.Account.create()
-        my_mnem = get_mnemonic(acct)
-        mnemonics.append(my_mnem + '\n')
+        mnemonic_phrase = generate_random_mnemonic()
+        print('phrase: ', mnemonic_phrase)
+        mnemonics.append(mnemonic_phrase + '\n')
         with open(filename, 'w') as file:
             file.writelines(mnemonics)
     else:
-        # Use existing mnemonic to recreate the account
-        my_mnem = mnemonics[keyId].strip()
-        acct = eth_account.Account.from_mnemonic(my_mnem)
+        print('we are in the else')
+        # Use an existing mnemonic to recreate the private key
+        mnemonic_phrase = mnemonics[keyId].strip()
+
+    # Derive the Ethereum address from the mnemonic phrase
+    private_key = w3.eth.account.from_mnemonic(mnemonic_phrase).privateKey
+    eth_addr = w3.eth.account.privateKeyToAccount(private_key).address
 
     # Sign the challenge
-    msg = eth_account.messages.encode_defunct(challenge)
-    sig = acct.sign_message(msg)
+    msg = w3.eth.account.encode_defunct(challenge)
+    sig = w3.eth.account.signHash(msg, private_key=private_key)
 
-    eth_addr = acct.address
-    assert eth_account.Account.recover_message(msg, signature=sig.signature.hex()) == eth_addr, "Failed to sign message properly"
+    assert w3.eth.account.recoverHash(msg, signature=sig.signature.hex()) == eth_addr, "Failed to sign message properly"
 
     return sig, eth_addr
 
